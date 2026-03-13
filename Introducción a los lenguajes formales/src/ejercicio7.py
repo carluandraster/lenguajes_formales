5d23335a6bcccbea4d2c5be0b53aa3d376d8fca1
carluandraster <andres.efstratiadis@gmail.com>
2026-03-12 22:49:29 -0300

    Update ejercicio7.py

--- Introducción a los lenguajes formales/src/ejercicio7.py
+++ Introducción a los lenguajes formales/src/ejercicio7.py
@@ -2,3 +2,12 @@ from Sarta import Sarta
 from Lenguaje import Lenguaje
 from ReglaDeProduccion import ReglaDeProduccion
 from Gramatica import Gramatica
+
+G = Gramatica(
+    "S",
+    Lenguaje({"S"}),
+    Lenguaje({"a", "b", "c", "0", "1"}),
+    {
+        ReglaDeProduccion("S", {Sarta("a"), Sarta("b"), Sarta("c"), Sarta("Sa"), Sarta("Sb"), Sarta("Sc"), Sarta("S0"), Sarta("S1")})
+    }
+)
